84e7a45f8e28abc241fdfe04a4eb0c0a9e60c552
Paulaaatp <p.torresp@uniandes.edu.co>
2026-02-19 19:07:36 -0500

    limpieza inicial datos

--- Proyecto1.py
+++ Proyecto1.py
@@ -2,6 +2,23 @@ import pandas as pd
 
 df = pd.read_csv("datos_cauca.csv")
 
+df = df.dropna(subset=[
+    "punt_ingles",
+    "punt_matematicas",
+    "punt_sociales_ciudadanas",
+    "punt_c_naturales",
+    "punt_lectura_critica"
+])
+
+df["punt_global"] = df["punt_global"].fillna(
+    (df[
+        ["punt_ingles",
+         "punt_matematicas",
+         "punt_sociales_ciudadanas",
+         "punt_c_naturales",
+         "punt_lectura_critica"]
+     ].sum(axis=1) / 13) * 5
+)
+
+print(df.shape) 
 print(df.head())
-
-
