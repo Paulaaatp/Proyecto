cfe88874b73f17a78821b7c84e1abc9d3622dc7d
Paulaaatp <p.torresp@uniandes.edu.co>
2026-02-20 20:30:33 -0500

    Mas limpieza de datos y nuevas variables

--- Proyecto1.py
+++ Proyecto1.py
@@ -2,9 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("datos_cauca.csv")
-print(df.head())
-print(df.shape)
+df_original = df.copy()
+df.columns = df.columns.str.lower().str.strip()
 
+# Eliminar faltantes y duplicados
+df = df.drop_duplicates()
 df = df.dropna(subset=[
     "punt_ingles",
     "punt_matematicas",
@@ -13,20 +15,53 @@ df = df.dropna(subset=[
     "punt_lectura_critica"
 ])
 
+# Calcular puntaje de icfes
 df["punt_global"] = df["punt_global"].fillna(
-    (df[
-        ["punt_ingles",
-         "punt_matematicas",
+    ((df[
+        ["punt_matematicas",
          "punt_sociales_ciudadanas",
          "punt_c_naturales",
          "punt_lectura_critica"]
-     ].sum(axis=1) / 13) * 5
+     ].sum(axis=1)*3 + df["punt_ingles"] )/ 13) * 5
 )
 
-print(df.shape) 
-print(df.head())
-df.columns
+
+cols_areas = [
+    "punt_ingles",
+    "punt_matematicas",
+    "punt_sociales_ciudadanas",
+    "punt_c_naturales",
+    "punt_lectura_critica"
+]
+
+# Validar rangos y tipo de datos
+for col in cols_areas:
+    df[col] = pd.to_numeric(df[col], errors="coerce")
+    df = df[(df[col] >= 0) & (df[col] <= 100)]
+
 df["punt_global"] = pd.to_numeric(df["punt_global"], errors="coerce")
+df = df[(df["punt_global"] >= 0) & (df["punt_global"] <= 500)]
+
+# Nueva variable de clasificación cualitativa por cuartiles
+df["nivel_global"] = pd.qcut(
+    df["punt_global"],
+    4,
+    labels=["Bajo", "Medio", "Alto", "Muy Alto"],
+    duplicates="drop"
+)
+
+# Nueva variable promedio por áreas
+df["promedio_areas"] = df[cols_areas].mean(axis=1)
+
+# Reporte de impacto
+print("Tamaño final:",df.shape) 
+print("Encabezado DataFrame:",df.head())
+print("Registros iniciales:", df_original.shape[0])
+print("Tomas con datos faltantes:", df_original.isna().sum())
+print("Registros finales:", df.shape[0])
+print("Porcentaje eliminado:", 
+      round((1 - df.shape[0]/df_original.shape[0]) * 100, 2), "%")
+print("Duplicados:", df_original.duplicated().sum())
 
 
 #######PREGUNTA 1
@@ -50,4 +85,13 @@ plt.title("Desigualdad interna por municipio - Cauca")
 plt.xlabel("Municipio")
 plt.ylabel("Desviación estándar del puntaje global")
 plt.show()
+
+
+plt.figure()
+df.boxplot(column="punt_global", by="estu_mcpio_reside")
+plt.xticks(rotation=45)
+plt.title("Dispersión del puntaje global - Cauca")
+plt.suptitle("")
+plt.show()
+
 ########
